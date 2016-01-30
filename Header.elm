713084b3cce16b72afeb463824c053452c584a15
Michael Gilliland <mjg.py3@gmail.com>
2016-01-29 21:12:03 -0500

    Better style for header

--- Header.elm
+++ Header.elm
@@ -1,6 +1,6 @@
 module Header (Action, Model, view, init, update) where
 
-import Html exposing (div, button, text)
+import Html exposing (div, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
@@ -17,19 +17,61 @@ items =
 
 itemAsHtml : Signal.Address Action -> Model -> (String, Model) -> Html.Html
 itemAsHtml address selected (value, option) =
-  div []
-    [button [onClick address { selectedModel = option }] [text (if option == selected then value ++ "*" else value)]]
+  let
+    style = if option == selected
+            then selectedItemStyle
+            else itemStyle
+  in
+    div [style, onClick address { selectedModel = option }]
+      [div
+        []
+        [text value]]
+
+selectedItemStyle : Html.Attribute
+selectedItemStyle =
+  baseItemStyle
+    [
+      ("color", "#970202")
+    , ("text-shadow", "2px 3px 5px rgba(60,50,9,0.3)")
+    ]
+
+itemStyle : Html.Attribute
+itemStyle =
+  baseItemStyle
+    [
+      ("color", "#F02424")
+    , ("text-shadow", "2px 3px 5px rgba(0,0,9,0.3)")
+    ]
+
+baseItemStyle : List (String, String) -> Html.Attribute
+baseItemStyle styles =
+  style
+    ([
+      ("height", "50px")
+    , ("padding-left", "10px")
+    , ("padding-right", "10px")
+    , ("font-weight", "bold")
+    , ("font-size", "x-large")
+    , ("background", "#585858")
+    , ("display", "inline-block")
+    , ("position", "relative")
+    , ("left", "40%")
+    , ("border-radius", "5px")
+    , ("margin", "10px")
+    , ("text-align", "center")
+    , ("cursor", "pointer")
+    ] ++ styles)
 
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
-  div [headerStyle] (List.map (itemAsHtml address model) items)
+  div [headerStyle] <| List.map (itemAsHtml address model) items
 
 headerStyle : Html.Attribute
 headerStyle =
   style
     [
       ("width", "100%")
-    , ("height", "60px")
+    , ("height", "70px")
     , ("background", "black")
     ]
 
