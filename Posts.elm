b0213a87fc4e2535f407f772142699bd095fbc71
Michael Gilliland <mjg.py3@gmail.com>
2016-01-30 10:31:59 -0500

    More work on posts. They're getting there :)

--- Posts.elm
+++ Posts.elm
@@ -1,8 +1,39 @@
-module Posts where
+module Posts (view) where
+
+import Html exposing (div, h1, text)
 
 import Html
 import Post
 import Posts.ObjectOrientedClojureExample
+import Dict
+
+type alias Model =
+  Maybe
+  { selectedPostId : Int
+  }
+
+init : Model
+init = Nothing
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model = div [] []
+
+type Action = MoveToList | SelectPost Int
+
+update : Action -> Model -> Model
+update action model = case action of
+  MoveToList -> init
+  SelectPost id -> Just { selectedPostId = id }
+
+monthYearToPost : Dict.Dict (Int, Int) (List (Post.Post, Html.Html))
+monthYearToPost =
+  let
+    getWithDefault def k d = Maybe.withDefault def <| Dict.get k d
+    reducer ((Post.BlogPost _ _ (year, month, _) _, _) as post) r =
+      let key = (year, month)
+      in Dict.insert key (post::getWithDefault [] key r) r
+  in
+     List.foldr reducer Dict.empty allPosts
 
 allPosts : List (Post.Post, Html.Html)
 allPosts =
