de82025442db98ab7df8d236674832d560a90f4a
Michael Gilliland <mjg.py3@gmail.com>
2016-02-20 20:19:48 -0500

    For now, undo tags as links.
    I need to try this again when I have a fresh set of eyes.

--- Tags.elm
+++ Tags.elm
@@ -2,14 +2,12 @@ module Tags where
 
 import Tag exposing (..)
 
-import CommonElements
-
 import Html exposing (text, i)
 
 tagsToHtml : List Tag -> Html.Html
 tagsToHtml tags =
   tags |>
-  List.map (CommonElements.internalLink [] << singleton << text << tagToString) |>
+  List.map (text << tagToString) |>
   List.intersperse (text ", ") |>
   (::) (text "Tags: ") |>
   i []
